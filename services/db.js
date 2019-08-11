2f0f3672ab0d602326ca570529b1ec044dad6eed
banobepascal <banobepascal@gmail.com>
2019-08-11 15:17:11 +0300

    modified db schema

--- services/db.js
+++ services/db.js
@@ -14,3 +14,24 @@ const pool = new pg.Pool(config);
 pool.on('connect', () => {
     console.log('connected to the Database');
 });
+
+const createTables = () => {
+    const schoolTable = `CREATE TABLE IF NOT EXISTS
+        students(
+            id SERIAL PRIMARY KEY,
+            student_name VARCHAR(128) NOT NULL,
+            student_age INT NOT NULL,
+            student_class VARCHAR(128) NOT NULL,
+            parent_contact VARCHAR(128) NOT NULL,
+            admission_date VARCHAR(128) NOT NULL
+        )`;
+         pool.query(schoolTable)
+         .then((res) => {
+             console.log(res);
+             pool.end();
+         })
+         .catch((err) => {
+             console.log(err);
+             pool.end();
+         });
+};
